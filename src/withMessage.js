5857adfe7a6cca31d4d95ac61efbdeb1103c1e50
Mark Davison <hello@poggle.co.uk>
2023-02-18 11:29:38 +0000

    Add support for custom prefixes

--- src/withMessage.js
+++ src/withMessage.js
@@ -38,7 +38,7 @@ const wrapMatcher = (matcher, customMessage, config) => {
       const matcherMessage =
         typeof error.matcherResult.message === 'function' ? error.matcherResult.message() : error.matcherResult.message;
 
-      const messagePrefix = config.showPrefix ? 'Custom message:\n  ' : '';
+      const messagePrefix = config.showPrefix ? config.prefixText : '';
 
       const message = () => messagePrefix + customMessage + (config.showMatcherMessage ? '\n\n' + matcherMessage : '');
       const e = new JestAssertionError({ ...matcherResult, message }, newMatcher);
@@ -67,14 +67,15 @@ const wrapMatchers = (matchers, customMessage, config) => {
   }, {});
 };
 
-export default (expect) => {
+export default (expect, prefixText) => {
   // proxy the expect function
   let expectProxy = Object.assign(
     (actual, customMessage, options = {}) => {
       const config = {
         showMatcherMessage: typeof options.showMatcherMessage === 'boolean' ? options.showMatcherMessage : true,
         showPrefix: typeof options.showPrefix === 'boolean' ? options.showPrefix : true,
-        showStack: typeof options.showStack === 'boolean' ? options.showStack : true
+        showStack: typeof options.showStack === 'boolean' ? options.showStack : true,
+        prefixText: typeof prefixText === 'string' ? prefixText : 'Custom message:\n  ',
       };
       let matchers = expect(actual); // partially apply expect to get all matchers and chain them
       if (customMessage) {
